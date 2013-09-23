cbf010fa1a57f619b6719edc103dd648e62a8d51
dmegginson-dell <daniel_megginson@dell.com>
2013-09-23 12:43:55 -0400

    Adding a console application.

---
+++ Console.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TC.Test.Build.Library.A;
+
+namespace TC.Test.Build.Console.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TC.Test.Build.Library.A.Bar bar = new TC.Test.Build.Library.A.Bar();
+
+            bar.Name = "Babar";
+
+            System.Console.WriteLine(bar.Name);
+        }
+    }
+}
